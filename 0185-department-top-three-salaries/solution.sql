14fcef0bd661d5a638811701c25bca09bc4d3181
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-07-27 22:42:57 +0000

    Sync LeetCode submission Runtime - 1857 ms (5.07%), Memory - 0.0B (100.00%)

---
+++ 0185-department-top-three-salaries/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+select x.Department, x.Employee, x.Salary
+from
+(select b.name as Department, a.name as Employee, a.Salary, 
+dense_rank() over(partition by a.departmentID order by a.salary desc) as r_no
+from Employee a join Department b
+on a.departmentID = b.id) x
+where x.r_no <= 3
+
