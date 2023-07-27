b83e014b3aa1c355a928967dc82441c1ad63c347
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-07-27 23:05:42 +0000

    Sync LeetCode submission Runtime - 565 ms (7.78%), Memory - 0.0B (100.00%)

---
+++ 0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select a.name
+from Employee a join 
+(select managerId, count(*) as cnt
+from Employee group by managerID) b
+on a.id = b.managerID
+where b.cnt >= 5
