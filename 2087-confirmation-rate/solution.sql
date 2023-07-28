2c094c7b25bfb048bfbb9d0267db359ca7b43c50
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-07-28 02:38:57 +0000

    Sync LeetCode submission Runtime - 1278 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ 2087-confirmation-rate/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+select c.user_id, coalesce(round((b.confirmed/a.total),2), 0) as confirmation_rate
+from
+Signups c left join 
+(select user_id, coalesce(count(*),0) as total 
+from Confirmations 
+group by user_id) a on c.user_id = a.user_id
+left join
+(select user_id, coalesce(count(*),0) as confirmed
+from Confirmations 
+where action = 'confirmed' 
+group by user_id) b
+on a.user_id = b.user_id
