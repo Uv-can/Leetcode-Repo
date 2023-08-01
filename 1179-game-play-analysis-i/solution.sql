6f758c412731204f97878b4641594e7476f98f9e
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 17:33:47 +0000

    Sync LeetCode submission Runtime - 1018 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ 1179-game-play-analysis-i/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select a.player_id, a.event_date as first_login
+from
+(select player_id, event_date,
+row_number() over(partition by player_id order by event_date) as r_no
+from Activity) a
+where a.r_no = 1
