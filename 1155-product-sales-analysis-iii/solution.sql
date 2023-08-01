108138d5f9948ce05d496cc213e998d59d0129fe
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 00:16:07 +0000

    Sync LeetCode submission Runtime - 2189 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 1155-product-sales-analysis-iii/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select a.product_id, a.year as first_year, a.quantity, a.price
+from
+(select product_id, year, quantity, price,
+dense_rank() over (partition by product_id order by year) as rnk
+from sales 
+) a where a.rnk = 1
+
+
+
