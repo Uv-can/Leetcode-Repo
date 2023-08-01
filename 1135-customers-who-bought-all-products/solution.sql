ff33f26d426f4e23683ebde52f40df50a24a7a69
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 00:31:51 +0000

    Sync LeetCode submission Runtime - 1135 ms (5.02%), Memory - 0.0B (100.00%)

---
+++ 1135-customers-who-bought-all-products/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select a.customer_id
+from
+(select customer_id, count(distinct product_key) as pro_cnt
+from Customer group by customer_id) a cross join
+(select count(distinct product_key) as pro_cnt from Product ) b
+where a.pro_cnt = b.pro_cnt
