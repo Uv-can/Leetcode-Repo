9e6ce82473f914cba3c3d60bf33641c9061f89bc
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 04:46:00 +0000

    Sync LeetCode submission Runtime - 2787 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ 1480-movie-rating/solution.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+(select c.name as results from(
+select b.name, count(*) as rating_cnt
+from MovieRating a left join
+Users b on
+a.user_id = b.user_id
+group by b.name
+order by rating_cnt desc, b.name asc limit 1) c)
+UNION all
+(select d.title as results from
+(select b.title,
+round(avg(a.rating) over(partition by a.movie_id ), 2) as avg_rating
+from MovieRating a left join
+Movies b on
+a.movie_id = b.movie_id
+where a.created_at like '2020-02-%') d
+order by d.avg_rating desc, d.title asc limit 1)
+
