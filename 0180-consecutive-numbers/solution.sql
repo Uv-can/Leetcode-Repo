05b756c28fa39338543cc8bb250b388f89374d31
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 01:07:54 +0000

    Sync LeetCode submission Runtime - 962 ms (6.37%), Memory - 0.0B (100.00%)

---
+++ 0180-consecutive-numbers/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select distinct a.num as ConsecutiveNums
+from
+(select num,
+lead(num) over() as next,
+lead(num,2) over() as second_next
+from Logs) a
+where a.num = a.next and a.num = a.second_next
