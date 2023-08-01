58d0f460cbe121dcbdac926a01cbcc7e0a51be04
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 17:12:26 +0000

    Sync LeetCode submission Runtime - 1652 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select a.customer_number
+from
+(select customer_number, count(*) as cnt
+from Orders 
+group by customer_number order by cnt desc
+limit 1) a
+
+
