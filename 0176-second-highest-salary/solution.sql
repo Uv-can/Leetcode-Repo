317fb7d1b8b890ae7dac2db89e990bd020a5180b
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 01:28:56 +0000

    Sync LeetCode submission Runtime - 570 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ 0176-second-highest-salary/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select 
+(select 
+distinct a.salary 
+from
+(select *, 
+dense_rank() over (order by salary desc) as rnk
+from Employee ) a
+where a.rnk = 2) as SecondHighestSalary
