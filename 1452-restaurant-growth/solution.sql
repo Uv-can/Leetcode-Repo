61164ad5b8be7019b12ad857be5b372630256b75
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 04:26:21 +0000

    Sync LeetCode submission Runtime - 737 ms (5.02%), Memory - 0.0B (100.00%)

---
+++ 1452-restaurant-growth/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+select b.visited_on, b.amount, b.average_amount
+from
+(select a.visited_on, 
+SUM(a.amount) over( rows between 6 preceding and current row) as amount,
+round(AVG(a.amount) over( rows between 6 preceding and current row),2) as average_amount,
+row_number() over(order by a.visited_on) as rnk
+from 
+(select visited_on, SUM(amount) as amount from Customer
+group by visited_on) a) b
+where b.rnk >= 7
+
