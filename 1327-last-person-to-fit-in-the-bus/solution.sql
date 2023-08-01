b3e516efc78df65a8be4eb82a07f2b92149eb1a8
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 05:35:03 +0000

    Sync LeetCode submission Runtime - 1453 ms (24.11%), Memory - 0.0B (100.00%)

---
+++ 1327-last-person-to-fit-in-the-bus/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select a.person_name
+from 
+(select person_name,
+SUM(weight) over(order by turn rows between unbounded preceding and current row ) as cum_sum
+from Queue ) a
+where a.cum_sum <= 1000 
+order by a.cum_sum desc limit 1
+
