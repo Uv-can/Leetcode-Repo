9c887649540f13870b8b8ffb4a39140cb369d037
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 17:44:36 +0000

    Sync LeetCode submission Runtime - 876 ms (5.11%), Memory - 0.0B (100.00%)

---
+++ 1291-immediate-food-delivery-i/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select round((a.fast_cnt/a.total)*100, 2) as immediate_percentage
+from
+(select count(*) as total ,
+SUM(case when order_date = customer_pref_delivery_date then 1 else 0 end) as fast_cnt
+from Delivery) a
+
