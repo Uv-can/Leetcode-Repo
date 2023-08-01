e5683b773f33b0fc5425977ab471f9fb8cf33424
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-01 03:52:09 +0000

    Sync LeetCode submission Runtime - 939 ms (7.53%), Memory - 0.0B (100.00%)

---
+++ 0585-investments-in-2016/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select round(SUM(tiv_2016),2) as tiv_2016
+from
+(select *,
+count(*) over(partition by tiv_2015 ) as tiv_2015_cnt,
+count(*) over(partition by lat, lon) as location_cnt
+from Insurance) a
+where a.tiv_2015_cnt > 1 and a.location_cnt = 1
