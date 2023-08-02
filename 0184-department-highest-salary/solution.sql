4e1109846cfae8d50b4855a79805ae8349a3fef0
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-02 23:33:49 +0000

    Sync LeetCode submission Runtime - 1640 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 0184-department-highest-salary/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select d.name as Department, a.name as Employee, a.salary as Salary
+from
+(select departmentId, name, salary,
+dense_rank() over(partition by departmentId order by salary desc) as rnk
+from Employee) a join Department d
+on a.departmentId = d.id 
+where a.rnk = 1 
