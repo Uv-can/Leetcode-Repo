6f1843c6ae66f53bee017d377b401a0506ff2470
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-02 17:32:15 +0000

    Sync LeetCode submission Runtime - 2069 ms (5.00%), Memory - 0.0B (100.00%)

---
+++ 1523-capital-gainloss/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select distinct a.stock_name, b.sell_price - a.buy_price as capital_gain_loss
+from
+(select stock_name,
+SUM(price) over(partition by stock_name ) as buy_price
+from Stocks where operation = 'Buy') a join 
+(select stock_name,
+SUM(price) over(partition by stock_name ) as sell_price
+from Stocks where operation = 'Sell') b on a.stock_name = b.stock_name
