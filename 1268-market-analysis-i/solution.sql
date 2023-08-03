a798c1cbce7e2c5019853e63f005bea52f66dfd9
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-03 15:03:16 +0000

    Sync LeetCode submission Runtime - 2727 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 1268-market-analysis-i/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select a.user_id as buyer_id, a.join_date,
+case when b.buyer_id is NULL then 0 else b.cnt end as orders_in_2019
+from Users a left join
+(select buyer_id , count(*) as cnt from Orders 
+where order_date like '2019%' group by buyer_id)b on a.user_id = b.buyer_id
+
