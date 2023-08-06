d8d2264c31d3f26d0664e406c61ffadc09cfc5de
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2023-08-06 00:18:11 +0000

    Sync LeetCode submission Runtime - 69 ms (5.58%), Memory - 17 MB (100.00%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -3,6 +3,12 @@ class Solution:
         char = ''
         
         for i in s:
-            if i.isalnum():
+            if self.isnumal(i):
                 char+=i.lower()
         return char == char[::-1] 
+
+    def isnumal(self, char):
+        return  (ord('a') <= ord(char) <= ord('z') or
+                 ord('A') <= ord(char) <= ord('Z') or
+                 ord('0') <= ord(char) <= ord('9'))
+
