e64113c33a6e15e5592872fb554f851587e249ef
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2024-04-20 23:33:11 +0000

    Sync LeetCode submission Runtime - 635 ms (12.49%), Memory - 0.0B (100.00%)

--- 0181-employees-earning-more-than-their-managers/solution.sql
+++ 0181-employees-earning-more-than-their-managers/solution.sql
@@ -2,8 +2,8 @@
 
 
 select a.name as Employee
-from Employee a join 
-(select id, salary from Employee ) b
-on a.managerId = b.id
+from 
+Employee a inner join Employee b
+on a.managerId = b.id 
 where a.salary > b.salary
 
