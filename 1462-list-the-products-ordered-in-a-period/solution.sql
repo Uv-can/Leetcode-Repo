4446c2076e02a32550daaf2db34e419188b45a73
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2024-04-21 04:06:08 +0000

    Sync LeetCode submission Runtime - 1253 ms (5.68%), Memory - 0.0B (100.00%)

--- 1462-list-the-products-ordered-in-a-period/solution.sql
+++ 1462-list-the-products-ordered-in-a-period/solution.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
-
-
-select a.product_name, b.unit 
-from 
-Products a left join 
-(select product_id, SUM(unit) as unit
-from orders 
-where order_date like '2020-02-%'
-group by product_id) b
-on a.product_id = b.product_id
-where unit >= 100
+select a.product_name, b.unit
+from Products a left join
+(select distinct product_id, 
+sum(unit) over(partition by product_id) as unit 
+from Orders where month(order_date) = 2 and year(order_date) = 2020) b
+on a.product_id = b.product_id 
+Where b.unit >= 100
