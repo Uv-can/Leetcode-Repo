37258236e919a72510dacae0e44d929d7b5f98bb
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-11 02:19:25 +0000

    Sync LeetCode submission Runtime - 201 ms (54.17%), Memory - 0.0B (100.00%)

--- 0619-biggest-single-number/solution.sql
+++ 0619-biggest-single-number/solution.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select max(a.num) as num
+select coalesce(max(num), null) as num
 from(
-select  num
-from MyNumbers 
-group by num having count(*) = 1)a
-
+select max(num) as num
+from mynumbers
+group by num
+having count(num) < 2)
