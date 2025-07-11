b3d0969108de88972e85f0de5970d9c6bf51f8cf
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-11 01:52:33 +0000

    Sync LeetCode submission Runtime - 474 ms (68.29%), Memory - 0.0B (100.00%)

--- 1258-article-views-i/solution.sql
+++ 1258-article-views-i/solution.sql
@@ -1,8 +1,6 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
-
-select author_id as id
-from Views 
-where author_id = viewer_id
-group by author_id
-order by author_id
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id=viewer_id 
+ORDER BY author_id ASC;
