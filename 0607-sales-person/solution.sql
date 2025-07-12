7606a75303f470e3b0a187844a237abd4c7309a0
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-12 18:44:40 +0000

    Sync LeetCode submission Runtime - 356 ms (46.47%), Memory - 0.0B (100.00%)

--- 0607-sales-person/solution.sql
+++ 0607-sales-person/solution.sql
@@ -1,9 +1,16 @@
-# Write your MySQL query statement below
-select name
-from SalesPerson
-where name not in  
-(select s.name
-    from SalesPerson s
-        left join Orders o on s.sales_id = o.sales_id
-        left join Company c on o.com_id = c.com_id
-    where c.name = 'Red')
+-- Write your PostgreSQL query statement below
+
+SELECT
+    s.name
+FROM
+    salesperson s
+WHERE
+    s.sales_id NOT IN (SELECT
+            o.sales_id
+        FROM
+            orders o
+                LEFT JOIN
+            company c ON o.com_id = c.com_id
+        WHERE
+            c.name = 'RED')
+;
