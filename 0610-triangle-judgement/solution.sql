49bec260050d9b42aca400556b2d6c0347473283
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-12 19:50:36 +0000

    Sync LeetCode submission Runtime - 276 ms (5.25%), Memory - 0.0B (100.00%)

--- 0610-triangle-judgement/solution.sql
+++ 0610-triangle-judgement/solution.sql
@@ -1,7 +1,5 @@
-# Write your MySQL query statement below
-
+-- Write your PostgreSQL query statement below
 
 select x,y,z,
-case when (x+y > z) and (x+z > y) and (y+z > x)
-then 'Yes' else 'No' end as triangle
-from Triangle
+case when (x+y >z and y+z > x and x+z > y) then 'Yes' else 'No' end as triangle
+from Triangle 
