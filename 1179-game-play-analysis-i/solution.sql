379a8fcd36c57e0686d4bb9269a1964927f6566f
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-12 00:34:51 +0000

    Sync LeetCode submission Runtime - 488 ms (76.22%), Memory - 0.0B (100.00%)

--- 1179-game-play-analysis-i/solution.sql
+++ 1179-game-play-analysis-i/solution.sql
@@ -1,8 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select a.player_id, a.event_date as first_login
-from
-(select player_id, event_date,
-row_number() over(partition by player_id order by event_date) as row_num
-from Activity) a
-where a.row_num = 1
+select player_id, min(event_date) as first_login
+from Activity
+group by player_id
