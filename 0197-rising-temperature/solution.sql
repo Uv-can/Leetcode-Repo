c29335e28c6b64384feeb8a835ec1b14bcf1cdae
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-12 17:02:58 +0000

    Sync LeetCode submission Runtime - 266 ms (40.62%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,5 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-# Write your MySQL query statement below
-select w1.id from Weather w1, Weather w2 where dateDiff(w1.recordDate,w2.recordDate) = 1 And w1.Temperature> w2.Temperature
 
+select w1.id
+from Weather w1 join Weather w2
+on w1.recorddate - w2.recorddate = 1 
+where w1.temperature > w2.temperature
