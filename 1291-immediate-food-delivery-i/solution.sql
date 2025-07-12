c19459c92a3f5b88b31e23f2c81d5e7793632eaa
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-12 01:01:00 +0000

    Sync LeetCode submission Runtime - 330 ms (100.00%), Memory - 0.0B (100.00%)

--- 1291-immediate-food-delivery-i/solution.sql
+++ 1291-immediate-food-delivery-i/solution.sql
@@ -1,9 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-
-select round((a.fast_cnt/a.total)*100, 2) as immediate_percentage
-from
-(select count(*) as total ,
-SUM(case when order_date = customer_pref_delivery_date then 1 else 0 end) as fast_cnt
-from Delivery) a
-
+Select 
+Round(avg(case when order_date=customer_pref_delivery_date then 1 else 0 end)*100,2) as immediate_percentage
+from delivery
