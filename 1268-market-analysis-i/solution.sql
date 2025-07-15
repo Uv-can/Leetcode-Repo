dc2ca2ba3c7604713cad353567465ec28cc00a09
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-15 13:46:13 +0000

    Sync LeetCode submission Runtime - 513 ms (83.07%), Memory - 0.0B (100.00%)

--- 1268-market-analysis-i/solution.sql
+++ 1268-market-analysis-i/solution.sql
@@ -1,9 +1,6 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-
-select a.user_id as buyer_id, a.join_date,
-case when b.buyer_id is NULL then 0 else b.cnt end as orders_in_2019
-from Users a left join
-(select buyer_id , count(*) as cnt from Orders 
-where order_date like '2019%' group by buyer_id)b on a.user_id = b.buyer_id
-
+select u.user_id as buyer_id, u.join_date, coalesce(count(order_id), 0) as orders_in_2019
+from users u left join orders o
+on u.user_id = o.buyer_id and extract(year from o.order_date) = 2019 
+group by u.user_id, u.join_date
