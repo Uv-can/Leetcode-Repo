070991b563d97922df962197dc3c721a3c9fe571
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-17 00:36:58 +0000

    Sync LeetCode submission Runtime - 828 ms (91.91%), Memory - 0.0B (100.00%)

--- 1155-product-sales-analysis-iii/solution.sql
+++ 1155-product-sales-analysis-iii/solution.sql
@@ -1,11 +1,7 @@
-# Write your MySQL query statement below
-
-select a.product_id, a.year as first_year, a.quantity, a.price
-from
-(select product_id, year, quantity, price,
-dense_rank() over (partition by product_id order by year) as rnk
-from sales 
-) a where a.rnk = 1
+-- Write your PostgreSQL query statement below
 
 
-
+select product_id, year as first_year, quantity, price 
+from sales
+where (product_id, year) in 
+(select product_id, min(year) from Sales group by product_id)
