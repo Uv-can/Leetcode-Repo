686dd560dd3160a3bb70bd38c9d3f208d58dd70c
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-18 02:18:58 +0000

    Sync LeetCode submission Runtime - 322 ms (20.24%), Memory - 0.0B (100.00%)

--- 0185-department-top-three-salaries/solution.sql
+++ 0185-department-top-three-salaries/solution.sql
@@ -1,11 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 
-select x.Department, x.Employee, x.Salary
-from
-(select b.name as Department, a.name as Employee, a.Salary, 
-dense_rank() over(partition by a.departmentID order by a.salary desc) as r_no
-from Employee a join Department b
-on a.departmentID = b.id) x
-where x.r_no <= 3
+select Department, Employee, Salary
+from (
+select d.name as Department, e.name as Employee, e.salary,
+dense_rank() over(partition by e.departmentid order by e.salary desc) as rnk
+from employee e join department d
+on e.departmentid = d.id) a
+where rnk <= 3
+
 
