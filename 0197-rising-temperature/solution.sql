f26a17f6fc8edee601eeff13dc1c7c857359c754
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-19 22:32:22 +0000

    Sync LeetCode submission Runtime - 239 ms (53.55%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,7 +1,27 @@
 -- Write your PostgreSQL query statement below
 
 
-select w1.id
-from Weather w1 join Weather w2
-on w1.recorddate - w2.recorddate = 1 
-where w1.temperature > w2.temperature
+--select w1.id
+--from Weather w1 join Weather w2
+--on w1.recorddate - w2.recorddate = 1 
+--where w1.temperature > w2.temperature
+
+WITH PreviousWeatherData AS
+(
+    SELECT 
+        id,
+        recordDate,
+        temperature, 
+        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
+        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
+    FROM 
+        Weather
+)
+SELECT 
+    id 
+FROM 
+    PreviousWeatherData
+WHERE 
+    temperature > PreviousTemperature
+AND 
+    recordDate = DATE_ADD(PreviousRecordDate, INTERVAL '1 DAY');
