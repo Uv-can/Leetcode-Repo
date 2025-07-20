513c948a9c1f73456fde1e870df1f0ec81ed5961
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-20 22:00:59 +0000

    Sync LeetCode submission Runtime - 897 ms (63.77%), Memory - 0.0B (100.00%)

--- 1155-product-sales-analysis-iii/solution.sql
+++ 1155-product-sales-analysis-iii/solution.sql
@@ -1,7 +1,13 @@
 -- Write your PostgreSQL query statement below
 
 
-select product_id, year as first_year, quantity, price 
-from sales
-where (product_id, year) in 
-(select product_id, min(year) from Sales group by product_id)
+--select product_id, year as first_year, quantity, price 
+--from sales
+--where (product_id, year) in 
+--(select product_id, min(year) from Sales group by product_id)
+
+select product_id, first_year, quantity, price from
+(select product_id, year as first_year, quantity, price,
+dense_rank() over(partition by product_id order by year) as rnk
+from sales) a
+where a.rnk = 1
