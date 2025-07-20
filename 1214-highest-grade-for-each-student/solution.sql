f11da7a58035481a6d40751f7f9983bb022eb218
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-07-20 22:11:13 +0000

    Sync LeetCode submission Runtime - 523 ms (58.21%), Memory - 0.0B (100.00%)

--- 1214-highest-grade-for-each-student/solution.sql
+++ 1214-highest-grade-for-each-student/solution.sql
@@ -3,7 +3,7 @@
 select a.student_id, a.course_id, a.grade
 from
 (select student_id, course_id, 
-MAX(grade) over(partition by student_id) as grade,
+ grade,
 dense_rank() over(partition by student_id order by grade desc, course_id) as rnk
 from enrollments )a
 where a.rnk = 1
