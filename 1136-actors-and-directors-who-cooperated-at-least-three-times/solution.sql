dba8168142719b3a193648a1543b11ec5303948c
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-08-31 22:57:41 +0000

    Sync LeetCode submission Runtime - 410 ms (11.40%), Memory - 0.0B (100.00%)

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
@@ -1,7 +1,6 @@
 -- Write your PostgreSQL query statement below
 
-select distinct a.actor_id, a.director_id from
-(select actor_id, director_id,
-count(*) over (partition by actor_id, director_id ) as cnt
-from actordirector) a
-where a.cnt >= 3
+select actor_id, director_id 
+from actordirector
+group by actor_id, director_id
+having count(timestamp) >= 3
