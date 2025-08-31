5099831d1a744aebba153dabf4f80185005e8da5
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-08-31 23:17:38 +0000

    Sync LeetCode submission Runtime - 870 ms (84.96%), Memory - 0.0B (100.00%)

--- 1174-sales-analysis-iii/solution.sql
+++ 1174-sales-analysis-iii/solution.sql
@@ -1,11 +1,10 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-SELECT s.product_id, product_name
-FROM Sales s
-LEFT JOIN Product p
-ON s.product_id = p.product_id
-GROUP BY s.product_id
-HAVING MIN(sale_date) >= CAST('2019-01-01' AS DATE) AND
-       MAX(sale_date) <= CAST('2019-03-31' AS DATE)
- 
 
+select product_id, product_name
+from product 
+where (product_id, product_name) not in
+( select distinct p.product_id, p.product_name
+from product p left join sales s on p.product_id = s.product_id
+where s.sale_date not between '2019-01-01' and '2019-03-31' or s.product_id is NULL
+)
