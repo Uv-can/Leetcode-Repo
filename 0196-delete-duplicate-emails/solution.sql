2cdf204851918c34889c2146501b42cbf1e75421
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-02 03:28:04 +0000

    Sync LeetCode submission Runtime - 239 ms (62.49%), Memory - 0.0B (100.00%)

--- 0196-delete-duplicate-emails/solution.sql
+++ 0196-delete-duplicate-emails/solution.sql
@@ -1,11 +1,6 @@
 -- Write your PostgreSQL query statement below
-
-delete from Person
-where (id, email) in
-(
-    select p.id, p.email from
-    (select id, email,
-    dense_rank() over(partition by email order by id) as rnk
-    from Person) p
-    where p.rnk > 1
-)
+DELETE FROM person
+where (id, email) in ( select p1.id, p1.email from person p1,
+    person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id)
