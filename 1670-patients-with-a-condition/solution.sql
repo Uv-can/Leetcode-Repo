5cdad9411a10723cf330dc3e425d3a06a67ef94b
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-15 22:33:23 +0000

    Sync LeetCode submission Runtime - 169 ms (98.63%), Memory - 0.0B (100.00%)

--- 1670-patients-with-a-condition/solution.sql
+++ 1670-patients-with-a-condition/solution.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-
-select patient_id, patient_name, conditions 
-from Patients 
-where conditions like '% DIAB1%' or conditions like 'DIAB1%'
+select * from patients
+where conditions like 'DIAB1%' or conditions like '% DIAB1%'
