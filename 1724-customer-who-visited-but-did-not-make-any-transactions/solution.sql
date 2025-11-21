f0ab7905f68d7ec43458e8ffbc08b81173c34346
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-21 00:43:09 +0000

    Sync LeetCode submission Runtime - 464 ms (82.57%), Memory - 0.0B (100.00%)

--- 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,8 +1,6 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-
-select a.customer_id, count(*) as count_no_trans
-from Visits a left join Transactions b
-on a.visit_id = b.visit_id 
-where b.visit_id is Null
-group by a.customer_id
+select v.customer_id, count(*) as count_no_trans
+from visits v left join transactions t on v.visit_id = t.visit_id
+where t.visit_id is null
+group by v.customer_id
