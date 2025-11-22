3562fbb4c3aa9e54bb9e073320679631009037d3
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-22 21:31:22 +0000

    Sync LeetCode submission Runtime - 322 ms (68.98%), Memory - 0.0B (100.00%)

--- 1882-the-number-of-employees-which-report-to-each-employee/solution.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select a.employee_id, a.name, b.reports_count, b.average_age
-from Employees a join
-(select reports_to, count(*) as reports_count, round(avg(age)) as average_age from Employees group by reports_to) b
-on a.employee_id = b.reports_to
-order by a.employee_id
 
+select e1.reports_to as employee_id, e2.name, count(distinct e1.employee_id) as reports_count, AVG(e1.age)::INTEGER as average_age
+from Employees e1 left join employees e2 
+on e1.reports_to = e2.employee_id
+where e1.reports_to is not null
+group by e1.reports_to, e2.name
