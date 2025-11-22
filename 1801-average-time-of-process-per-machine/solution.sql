198c6abd4c14bef6ece06b97c26711f79e7d8219
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-22 19:11:37 +0000

    Sync LeetCode submission Runtime - 167 ms (77.55%), Memory - 0.0B (100.00%)

--- 1801-average-time-of-process-per-machine/solution.sql
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -1,16 +1,16 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select x.machine_id, round(SUM(x.time_diff)/count(x.time_diff),3) as processing_time
-from
-(select a.machine_id, a.process_id, round((b.timestamp - a.timestamp),3) as time_diff
-from
-(select machine_id, process_id, activity_type, timestamp
-from Activity 
-where activity_type = 'start') a
-left join
-(select machine_id, process_id, activity_type, timestamp
-from Activity 
-where activity_type = 'end') b
-on a.machine_id = b.machine_id and a.process_id = b.process_id) x
-group by x.machine_id
 
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp)::NUMERIC, 3) AS processing_time
+FROM Activity a, 
+     Activity b
+WHERE 
+    a.machine_id = b.machine_id
+AND 
+    a.process_id = b.process_id
+AND 
+    a.activity_type = 'start'
+AND 
+    b.activity_type = 'end'
+GROUP BY a.machine_id
