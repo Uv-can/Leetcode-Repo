943e9fd799b9e4f68b58eb5a9971b81d6c52fd30
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-28 04:21:13 +0000

    Sync LeetCode submission Runtime - 263 ms (13.36%), Memory - 0.0B (100.00%)

---
+++ 2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select employee_id
+from employees 
+where manager_id not in (select distinct employee_id from employees) and salary < 30000
+order by 1
