3bd604bed195265f7768d25835f049e656d8729a
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2025-11-29 00:23:06 +0000

    Sync LeetCode submission Runtime - 518 ms (75.73%), Memory - 0.0B (100.00%)

--- 2495-number-of-unique-subjects-taught-by-each-teacher/solution.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/solution.sql
@@ -1,9 +1,6 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-select a.teacher_id,
-max(a.rnk) as cnt
-from
-(select teacher_id,
-dense_rank() over (partition by teacher_id order by subject_id) as rnk
-from Teacher order by teacher_id, rnk desc) a
-group by a.teacher_id
+
+select teacher_id, count(distinct subject_id) as cnt
+from teacher
+group by teacher_id
