2abf7f6c5dbfc2042e371cef691544420ceb3c90
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2026-07-26 22:39:17 +0000

    Sync LeetCode submission Runtime - 213 ms (96.15%), Memory - 0.0B (100.00%)

---
+++ 2224-drop-type-1-orders-for-customers-with-type-0-orders/solution.sql
@@ -0,0 +1,18 @@
+-- Write your PostgreSQL query statement below
+
+
+
+SELECT *
+FROM Orders
+WHERE order_type = 0
+
+UNION
+
+SELECT *
+FROM Orders
+WHERE order_type = 1
+    AND customer_id NOT IN (
+        SELECT customer_id
+        FROM Orders
+        WHERE order_type = 0
+);
