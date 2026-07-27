e23a1a0ac6734db9287fe27c19286e24d181bfb3
Indrajit Swami <55006162+Uv-can@users.noreply.github.com>
2026-07-27 02:21:22 +0000

    Sync LeetCode submission Runtime - 360 ms (70.00%), Memory - 0.0B (100.00%)

---
+++ 2431-products-with-three-or-more-orders-in-two-consecutive-years/solution.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+
+
+SELECT DISTINCT d.product_id 
+FROM
+(
+    SELECT product_id, 
+ extract(YEAR from purchase_date) AS curr_year,
+ LEAD(extract(YEAR from purchase_date)) OVER(PARTITION BY product_id ORDER BY extract(YEAR from purchase_date)) AS next_year
+ FROM orders
+ GROUP BY curr_year, product_id
+ HAVING COUNT(order_id) >= 3
+ ) d
+ WHERE d.next_year=d.curr_year+1
